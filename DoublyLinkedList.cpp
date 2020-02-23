e021b398309260dfda4c276c66ca3eef3412c1ea
Mario <m.villa.lore@gmail.com>
2020-02-23 20:14:58 +0100

    License added in cpp module

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -1,3 +1,23 @@
+/*Copyright [2020] [Mario Villa Lore]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+This is a simple implementation of various data structures in C++ for learning pourposes
+At 21-feb-2020 there is:
+ * Simple Linked List
+
+*/
+
 #include <iostream>
 #include "DoublyLinkedList.h"
 
